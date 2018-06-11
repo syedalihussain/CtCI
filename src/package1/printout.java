a53efb1bf4bea07672561abcd9c4e808340641a5
Syed Ali Hussain <u6028474@anu.edu.au>
2018-06-11 13:38:25 +1000

    First commit

---
+++ src/package1/printout.java
@@ -0,0 +1,13 @@
+package package1;
+import java.util.*;
+public class printout {
+	public static void changeArray(char[] charArray){
+		charArray[0] = 'b';
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 1, num2 = 2;
+		System.out.println(num1 | num2);
+	}
+
+}
---
+++ src/package1/rotateMatrix1_7.java
@@ -0,0 +1,46 @@
+package package1;
+
+public class rotateMatrix1_7 {
+	public static boolean rotateMatrix(int[][] matrix){
+		if (matrix.length==0 || matrix.length!=matrix[0].length) return false;
+		int n = matrix.length;
+		for (int layer=0; layer<n/2; layer++) {
+			int first = layer;
+			int last = n - 1 - layer;
+			for (int i=first; i<last; i++) {
+				int offset = i - first;
+				int top = matrix[first][i];
+				
+				//	left -> top
+				matrix[first][i] = matrix[last-offset][first];
+				
+				//	bottom -> left
+				matrix[last-offset][first] = matrix[last][last-offset];
+				
+				//	right -> bottom
+				matrix[last][last-offset] = matrix[i][last];
+				
+				//	top -> right
+				matrix[i][last] = top; // right <- saved top
+			}
+		}
+			
+		return true;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] multi = new int[][]{
+			  { 1, 2, 3 },
+			  { 4, 5, 6 },
+			  { 7, 8, 9 }
+			};
+		rotateMatrix(multi);
+		for (int i=0; i<multi.length; i++){
+			for (int j=0; j<multi[i].length; j++){
+				System.out.print(multi[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
