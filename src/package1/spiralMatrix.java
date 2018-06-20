68844e4e727154e2be5f5d6afcfbdc0a51d7b548
Syed Ali Hussain <u6028474@anu.edu.au>
2018-06-20 16:40:34 +1000

    unfinished business

--- src/package1/spiralMatrix.java
+++ src/package1/spiralMatrix.java
@@ -1,9 +1,27 @@
 package package1;
 
 public class spiralMatrix {
-	public static int[] traverse(int[][] m){
-//		int numCols = 
-//		for (int i=0; )
+	public static int[] traverse(int[][] matrix){
+		int n = matrix[0].length;
+		int m = matrix.length;
+		int rn = n--;
+		int rm = m--;
+		int[] arr = new int[m*n];
+		int c = 0, r = 0;
+		boolean east = true, south = false, west = false, north = false;
+		int index = 0;
+		while (){
+			for (int a=c; a<n; a++){
+				arr[index++] = matrix[r][a];
+			}
+			for (int b=r; b<m; b++){
+				arr[index++] = matrix[b][rn];
+			}
+			c++; r++;
+			for (int a=rn; a>c; a--){
+				arr[index++] = matrix[]
+			}
+		}
 		int[] temp = new int[]{1, 2, 3, 4 };
 		return temp;
 	}
@@ -17,6 +35,7 @@ public class spiralMatrix {
 		int[] res = traverse(matrix);
 		System.out.println(matrix.length);
 		System.out.println(matrix[0].length);
+		System.out.println(matrix[1][3]);
 		for (int i=0; i<res.length; i++){
 			System.out.print(res[i] + " ");
 		}
