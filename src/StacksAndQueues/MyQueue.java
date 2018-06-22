2df82d2312bc55ae1aece4f6ee17911220dfebe7
Syed Ali Hussain <u6028474@anu.edu.au>
2018-06-22 11:16:07 +1000

    Queue Implementation Complete

---
+++ src/StacksAndQueues/MyQueue.java
@@ -0,0 +1,45 @@
+package StacksAndQueues;
+
+import java.util.NoSuchElementException;
+
+public class MyQueue<T> {
+	private static class QueueNode<T>{
+		private T data;
+		private QueueNode<T> next;
+		public QueueNode(T data) {
+			this.data = data;
+		}
+	}
+	private QueueNode<T> head;
+	private QueueNode<T> tail;
+	
+	public void add(T item){
+		QueueNode<T> t = new QueueNode<T>(item);
+		if (tail!=null){
+			tail.next = t;
+		}
+		tail = t;
+		if (head==null){
+			head=tail;
+		}
+	}
+	
+	public T remove() {
+		if (head == null) throw new NoSuchElementException();
+		T data = head.data;
+		head = head.next;
+		if (head == null){
+			tail = null;
+		}
+		return data;
+	}
+	
+	public T peek() {
+		if (head == null) throw new NoSuchElementException();
+		return head.data;
+	}
+	
+	public boolean isEmpty(){
+		return head == null;
+	}
+}
